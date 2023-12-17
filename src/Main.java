53c01fdf7e95a118f821dac37d144dc6c6046cd8
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-12-16 21:28:30 -0600

    testing

---
+++ src/Main.java
@@ -0,0 +1,405 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!");
+//     }
+// }
+
+
+
+
+// *****
+
+// Widening Type Casting
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int myInt = 9;
+//       double myDouble = myInt; // Automatic casting: int to double
+  
+//       System.out.println(myInt);      // Outputs 9
+//       System.out.println(myDouble);   // Outputs 9.0
+//     }
+//   }
+
+
+// *****
+
+
+
+// Narrowing Type Casting - put smaller data type in parentheses in front of value
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       double myDouble = 9.78d;
+//       int myInt = (int) myDouble; // Manual casting: double to int
+  
+//       System.out.println(myDouble);   // Outputs 9.78
+//       System.out.println(myInt);      // Outputs 9
+//     }
+//   }
+
+// *****
+
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 100 + 50;
+//       System.out.println(x);
+//     }
+//   }
+
+// *****
+
+
+// Adding values and variables
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int sum1 = 100 + 50;
+//       int sum2 = sum1 + 250;
+//       int sum3 = sum2 + sum2;
+//       System.out.println(sum1);
+//       System.out.println(sum2);
+//       System.out.println(sum3);  
+//     }
+//   }
+  
+
+
+
+
+// *****
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 5;
+//       int y = 3;
+//       System.out.println(x - y);
+//     }
+//   }
+  
+
+
+
+
+
+
+
+// *****
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 5;
+//       int y = 3;
+//       System.out.println(x * y);
+//     }
+//   }
+  
+
+
+
+
+
+
+// *****
+
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 12;
+//       int y = 3;
+//       System.out.println(x / y);
+//     }
+//   }
+  
+
+
+
+// *****
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 5;
+//       int y = 2;
+//       System.out.println(x % y);
+//     }
+//   }
+  
+
+
+
+
+
+// *****
+
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 5;
+//       ++x;
+//       System.out.println(x);
+//     }
+//   }
+
+
+// *****
+
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 5;
+//       --x;
+//       System.out.println(x);
+//     }
+//   }
+  
+
+
+
+
+// *****
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 10;
+//       System.out.println(x);
+//     }
+//   }
+  
+
+
+
+
+
+// *****
+
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 10;
+//       x += 5;
+//       System.out.println(x);
+//     }
+//   }
+  
+
+
+
+
+// *****
+
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 5;
+//       x -= 3;
+//       System.out.println(x);
+//     }
+//   }
+  
+
+
+
+
+// *****
+
+
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 5;
+//       x *= 3;
+//       System.out.println(x);
+//     }
+//   }
+  
+
+
+
+// *****
+
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       double x = 5;
+//       x /= 3;
+//       System.out.println(x);
+//     }
+//   }
+  
+
+
+
+
+// *****
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 5;
+//       x %= 3;
+//       System.out.println(x);
+//     }
+//   }
+  
+
+
+
+
+// *****
+
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 5;
+//       x &= 3;
+//       System.out.println(x);
+//     }
+//   }
+  
+
+
+
+
+// *****
+
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 5;
+//       x |= 3;
+//       System.out.println(x);
+//     }
+//   }
+  
+
+
+
+
+// *****
+
+
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 5;
+//       x ^= 3;
+//       System.out.println(x);
+//     }
+//   }
+  
+
+
+
+// *****
+
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 5;
+//       x >>= 3;
+//       System.out.println(x);
+//     }
+//   }
+  
+
+
+
+
+
+// *****
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 5;
+//       x <<= 3;
+//       System.out.println(x);
+//     }
+//   }
+  
+
+
+
+
+
+
+// *****
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//       int x = 5;
+//       int y = 3;
+//       System.out.println(x > y); // returns true, because 5 is higher than 3
+//     }
+//   }
+  
+
+
+
+
+
+// *****
+
+
+
+
+
+
+
+
+// *****
+
+
+
+
+
+
+
+
+
+
+
+// *****
+
+
+
+
+
+
+
+
+// *****
+
+
+
+
+
+
+
+
+// *****
+
+
+
+
+
+
+
+
+// *****
